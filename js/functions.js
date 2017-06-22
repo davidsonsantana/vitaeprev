3cb7ca85554e6a9f20947a18578c5d00aa8826c3
Davidson Santana <davidsonjsantana@gmail.com>
2017-06-22 19:45:53 -0300

    Ajustes gerais e criacao da tela relatorio.

--- js/functions.js
+++ js/functions.js
@@ -7,5 +7,28 @@ $(function(){
   });
 
 
+  // Check ao inserir novo consultor
+  $('.form-group select.funcao').bind('change', function (e) { 
+    if( $('.form-group select.funcao').val() == 'consultor') {
+      $('.show-gerente').show();
+    } else{
+    	$('.show-gerente, .gerente').hide();
+    	$(".check-gerente").attr('checked', false);
+    }
+            
+  }).trigger('change');
+
+
+  $(".check-gerente").click(function() {
+    if($(this).is(":checked")) {
+        $(".gerente").show(300);
+    } else {
+        $(".gerente").hide(200);
+    }
+  });
+
+
+
+
   
 });
